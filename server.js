7f5c3bfe7a8336bb2a192e3c2448744a1c07ea83
ShaaCoder <125294108+ShaaCoder@users.noreply.github.com>
2024-12-09 18:24:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ import dotenv from 'dotenv';
 dotenv.config(); // Load environment variables from .env file
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
